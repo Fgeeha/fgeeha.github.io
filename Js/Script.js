059dcf23c967dd55eeef2c3625805ec0bb344ab6
Fgeeha <kolesnikov.nikitavlg@gmail.com>
2020-10-29 21:21:47 +0400

    V1.3 Time
    add Time

--- Js/Script.js
+++ Js/Script.js
@@ -41,4 +41,16 @@ function copyToClipboardDis(text){
 			    document.body.removeChild(copytext)
 			    alert("Discord id скопирован в буфер обмена")
 				}
-	
+
+
+
+setInterval(function () {  
+ date = new Date(),  
+ h = date.getHours(),  
+ m = date.getMinutes(),  
+ s = date.getSeconds(),  
+ h = (h < 10) ? '0' + h : h,  
+ m = (m < 10) ? '0' + m : m,  
+ s = (s < 10) ? '0' + s : s,  
+ document.getElementById('time').innerHTML = h + ':' + m + ':' + s;  
+}, 1000);
